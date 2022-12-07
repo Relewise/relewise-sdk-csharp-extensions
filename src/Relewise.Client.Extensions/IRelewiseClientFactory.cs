f788a4cd909f7fc674253c27a8719cdc234dd855
Brian Juul Andersen <brianmanden@gmail.com>
2022-12-07 11:40:17 +0100

    Update IRelewiseClientFactory.cs
    Fix minor grammatical errors

--- src/Relewise.Client.Extensions/IRelewiseClientFactory.cs
+++ src/Relewise.Client.Extensions/IRelewiseClientFactory.cs
@@ -5,7 +5,7 @@ namespace Relewise.Client.Extensions;
 
 /// <summary>
 /// The Service Locator / factory from where you can access all configured clients.
-/// In instance of this interface can be located through the <see cref="IServiceProvider"/> instance directly - or indirectly through dependency injection.
+/// An instance of this interface can be located through the <see cref="IServiceProvider"/> instance directly - or indirectly through dependency injection.
 /// </summary>
 public interface IRelewiseClientFactory
 {
@@ -19,17 +19,17 @@ public interface IRelewiseClientFactory
     /// <summary>
     /// Provides access to the options used to configure the specific client.
     /// </summary>
-    /// <typeparam name="TClient">Defines for which client you'd like to access options, e.g. <see cref="ITracker"/>.</typeparam>
+    /// <typeparam name="TClient">Defines which client you'd like to access options, e.g. <see cref="ITracker"/>.</typeparam>
     /// <param name="name">Optional parameter if you are accessing options for a named client.</param>
     RelewiseClientOptions GetOptions<TClient>(string? name = null) where TClient : class, IClient;
 
     /// <summary>Determines whether the factory contains the specified client.</summary>
-    /// <typeparam name="TClient">Defines which client, e.g. <see cref="ITracker"/>, you'd like check if it exists.</typeparam>
+    /// <typeparam name="TClient">Defines which client, e.g. <see cref="ITracker"/>, you'd like to check existence of.</typeparam>
     /// <param name="name">Optional parameter if you are accessing options for a named client.</param>
     bool Contains<TClient>(string? name = null) where TClient : class, IClient;
 
     /// <summary>
-    /// Returns a list of all the client names that was registered during StartUp. This can be used to loop over the clients to read their configured options.
+    /// Returns a list of all the client names that were registered during StartUp. This can be used to loop over the clients to read their configured options.
     /// </summary>
     IReadOnlyCollection<string> ClientNames { get; }
 }
